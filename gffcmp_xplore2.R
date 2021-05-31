1a73a48f796a6873fdf466942220a9450b545441
Geo Pertea <geo.pertea@gmail.com>
2021-05-30 20:35:49 -0400

    added gffcmp_class_codes ribo/polyA chart

---
+++ gffcmp_xplore2.R
@@ -0,0 +1,84 @@
+
+library(ggplot2)
+library(plotly)
+# load a tmap file
+tmapZ<-read.table('data/riboZ/cmpR3907.R3907_C3RGLACXX.gtf.tmap', 
+               header=T, sep='\t')
+gtitleZ <- "R3907 riboZ"
+
+tmapZ<-read.table('data/riboZ/cmpR3907.R3907_C3RGLACXX.gtf.tmap', 
+                  header=T, sep='\t')
+tmapZ<-subset(tmapZ, FPKM>1.0 & num_exons>1, 
+              select=c(ref_id,class_code,qry_id, num_exons, FPKM, TPM, len))
+gtitleZ <- "R3907 riboZ"
+
+tmapZ$prot <- gtitleZ
+
+tmapA<-read.table('data/polyA/cmpR3907.R3907_C101EACXX.gtf.tmap', 
+                  header=T, sep='\t')
+tmapA<-subset(tmapA, FPKM>1.0 & num_exons>1, 
+              select=c(ref_id,class_code,qry_id, num_exons, FPKM, TPM, len))
+gtitleA <- "R3907 polyA"
+
+tmapA$prot <- gtitleA
+
+tmap <- rbind(tmapA, tmapZ) 
+
+# filter the data to only multi-exon transcripts and only a few columns of interest
+##draw a bar chart:
+#ggplot(tmmap)+geom_bar(mapping=aes(class_code))
+##which is the same with
+#ggplot(tmmap)+geom_bar(aes(x=class_code))
+##this is the same with:
+#ggplot(tmmap, aes(class_code))+geom_bar()
+### 1) one way to add labels with actual counts above each bar:
+# is to use the data from the bar plot already
+g <- ggplot(tmmap)+geom_bar(aes(x=class_code))
+g_dt <- layer_data(g) #get layer data from the graph we just built
+#then use annotate():
+g+annotate(geom="text",label=g_dt$count, x=g_dt$x, y=g_dt$ymax+340)
+### 2) another way to add the count labels is to call stat_count() 
+### and use the ..count.. internals:
+#ggplot(tmmap)+geom_bar()+aes(class_code)+
+#  stat_count(aes(label=..count..), vjust=-0.5, geom="text", position="identity")
+## the above is the same with:
+#ggplot(tmmap, aes(class_code))+geom_bar()+
+#  stat_count(aes(label=..count..), vjust=-0.5, geom="text", position="identity")
+## which is also the same with:
+#ggplot(tmmap)+aes(class_code) + geom_bar() + 
+#  geom_text(aes(label=..count..), vjust=-0.5, stat="count", position="identity")
+## to hide the legend, colorize the bars and increase font:
+#ggplot(tmmap)+aes(class_code) + geom_bar(aes(fill=factor(class_code))) + 
+#  geom_text(aes(label=..count..), vjust=-0.5, stat="count", position="identity")+
+#  theme(text = element_text(size=16), legend.position = "none")
+##
+## Want to discard all class codes with less than 100 entries (u,y,o,i etc.)
+## while also changing the bar chart so the bars are sorted by frequency
+## Note that levels(factor(tmmap$class_code)) has the default order of the bars
+## so we have to change that
+## -- Create a new column with the desired ordering as given by table(tmap$class_code)
+## sort(table(tmmap$class_code), decreasing=TRUE) shows the desired order!
+## take names() of that to get just the list of class_codes in the order we want them, 
+## so the new factor column should be built like this
+## - we could sort by frequency
+#> tmmap$class <- factor(tmmap$class_code, 
+#                         levels=names(sort(table(tmmap$class_code), decreasing=TRUE)))
+### -- but let's use order or interest: 
+codePrio <- c('=','j','k','y','i','u','o','x','m','n','s')
+tmmap$class <- factor(tmmap$class_code, codePrio)
+## now the class column is of type factor, it shows the same class_code 
+## but the factor levels internally are different
+## the following should now show the class codes (class entries) in the order of 
+## decreasing frequency but also discarded if they are lower than 100
+#> levels(tmmap$class)[table(tmmap$class)>=100]
+## so now we can subset the data frame using this new class column:
+# tmsel <- subset(tmmap, class %in% levels(tmmap$class)[table(tmmap$class)>=100])
+tmsel <- subset(tmmap, class %in% levels(tmmap$class))
+# note the above is the same with this (the class only matters for sorting!):
+#tmsel <- subset(tmmap, class_code %in% levels(factor(tmmap$class_code))[table(tmmap$class_code)>=100])
+gp1 <- ggplot(tmsel)+aes(class) + geom_bar(aes(fill=class)) + 
+  geom_text(aes(label=..count..), vjust=-0.5, stat="count", position="identity")+
+  theme(text = element_text(size=16), legend.position = "none") + ggtitle(gtitle)
+#ggplotly(gp1)
+gp1
+
